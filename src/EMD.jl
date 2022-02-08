50d06b66c69b42b83e803db0d14f552abdf77076
Jordan Smith <46058723+jsmithnh09@users.noreply.github.com>
2022-02-07 19:21:44 -0500

    Update EMD.jl

--- src/EMD.jl
+++ src/EMD.jl
@@ -1,1 +1,13 @@
-using Interpolations
+"""
+Empirical Mode Decomposition Package.
+"""
+module EMD
+
+import DSP, Distributions, Interpolations
+
+## include environment setup.
+include("config.jl")
+
+EMD
+
+end # module
