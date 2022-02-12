da6739d79bc2fa60c933baf8ed0dc1d6027b1f05
Jordan S <46058723+jsmithnh09@users.noreply.github.com>
2022-02-12 11:48:07 -0500

    Adding base utility functions.

--- src/EMD.jl
+++ src/EMD.jl
@@ -4,6 +4,7 @@ Empirical Mode Decomposition Package.
 module EMD
 
 import DSP, Distributions, Interpolations
+using Statistics: mean
 
 ## include environment setup.
 include("config.jl")
--- src/config.jl
+++ src/config.jl
@@ -16,9 +16,67 @@ struct EMDConfig{T<:AbstractFloat}
     sift_h::Int
     taxis::StepRange
     stop::AbstractVector{T}
+    x::AbstractVector{T}
+    N::Int
     interp::InterpMode
     function EMDConfig(x::AbstractVector{T}) where T
-        t = StepRange(1, 1, length(x))
-        new{T}(Int(2_000), typemax(Int), Int(4), 0, 0, t, Array{T}([0.05, 0.5, 0.05]), Cubic)
+        N = length(x)
+        t = StepRange(1, 1, N)
+        new{T}(Int(2_000), typemax(Int), Int(4), 0, 0, t, Array{T}([0.05, 0.5, 0.05]), x, N, Cubic)
     end
 end
+
+"""
+    (tmin, tmax, zmin, zmax) = boundscheck(opts::EMDConfig, indmin::Array, indmax::Array, nbsym::Int)
+
+Boundary check to define extrema beyond the input signal limits to prevent boundary issues.
+Without mirror symmetry, we get ramping at either ends of the IMF signal.
+"""
+function boundscheck(indmin::AbstractVector, indmax::AbstractVector, opts::EMDConfig, z, nbsym::Int)
+    length(indmin) + length(indmax) ≥ 3 || error("EMD: boundscheck: not enough extrema.")
+    if (indmax[1] < indmin[1])
+        if (opts.x[1] > opts.x[indmin[1]])
+            # original EMD-m file used "end" keyword with minimum function??
+        end
+    end
+end
+
+"""
+   stop = stopemd(opts::EMDConfig, imf::AbstractVector)
+
+Returns a flag indicating if at least 3 extrema are present to continue 
+the decomposition.
+"""
+function stopemd(otps::EMDConfig, imf::AbstractVector)
+    (indmin, indmax) = extr(imf)
+    Bool(length(indmin) + length(indmax) < 3)
+end
+
+
+function stopsift(opts::EMDConfig, imf::AbstractVector)
+    (mean, npeaks, ndips, amp) = meanamplitude(opts, imf)
+    Sx = abs.(mean) ./ amp
+    S = mean(Sx)
+end
+
+"""
+   oind = orthoindex(opts::EMDConfig, imf::AbstractVector)
+
+`orthoindex` computes the index of orthogonality based on the input
+signal `x` and the prospective mode function `imf`.
+"""
+function orthoindex(opts::EMDConfig, imf::AbstractVector)
+    n = size(imf, 1)
+    s = 0
+    for i = 1:n
+        for j = 1:n
+            if i != j
+                # we can improve this with `muladd`...
+                s += abs(sum(imf[i,:]) .* conj.(imf[j,:]))/sum(x.^2)
+            end
+        end
+    end
+    0.5*s
+end
+    
+    
